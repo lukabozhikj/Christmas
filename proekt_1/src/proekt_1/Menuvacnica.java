447767ae6390f98cdff97d338a143183849d3e7b
lukabozhikj <147811366+lukabozhikj@users.noreply.github.com>
2023-10-16 23:18:52 +0200

    Add files via upload

---
+++ proekt_1/src/proekt_1/Menuvacnica.java
@@ -0,0 +1,14 @@
+package proekt_1;
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+	int a=236;
+	double x=a*61.7;
+	System.out.println("236 evra vo denari se:");
+	System.out.println(x);
+	
+
+	}
+
+}
---
+++ proekt_1/src/proekt_1/Smetka.java
@@ -0,0 +1,14 @@
+package proekt_1;
+
+public class Smetka {
+
+	public static void main(String[] args) {
+	int Jabolko = 52;
+	int Banani = 32;
+	double rezultat = (Jabolko * 2) + (Banani * 5);
+	System.out.println ("Vkupna smetka e:");
+	System.out.print (rezultat);
+	
+	}
+
+}
