32d898d736f1fc09c67b76addc135f1691538eed
Luka <147811366+lukabozhikj@users.noreply.github.com>
2023-10-26 17:32:01 +0200

    Add files via upload

---
+++ Proekt_6/Matematickaoperacija.java
@@ -0,0 +1,22 @@
+package Proekt_6;
+import java.math.*;
+import java.util.Scanner;
+public class Matematickaoperacija {
+    public static void main(String[] args) {
+	System.out.println(Math.max(20, 30));
+	
+	System.out.println(Math.min(20, 30));
+	
+    System.out.println(Math.PI);
+    
+    System.out.println(Math.decrementExact(5));
+    
+    System.out.println(Math.incrementExact(10));
+    
+    System.out.println(Math.log10(8));
+    
+    System.out.println(Math.floorMod(7, 5));
+    
+    System.out.println(Math.nextUp(10));
+    }
+}
